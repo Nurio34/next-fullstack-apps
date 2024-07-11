cf98fdada8a1495a726766013adf777da5e96c67
Nurio34 <nurioonsoftware@gmail.com>
2024-07-11 22:51:43 +0300

    bugFix_for_Vercel

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,19 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher([
-    "/todoApp(.*)",
-    "/instagram/(.+)",
-]);
+// const isProtectedRoute = createRouteMatcher([
+//     "/todoApp(.*)",
+//     "/instagram/(.+)",
+// ]);
 
-export default clerkMiddleware((auth, req) => {
-    console.log("Middleware invoked for route:", req.url); // Add logging
-    if (isProtectedRoute(req)) {
-        console.log("Protected route detected:", req.url); // Add logging
-        auth().protect();
-        return;
-    }
-});
+// export default clerkMiddleware((auth, req) => {
+//     console.log("Middleware invoked for route:", req.url); // Add logging
+//     if (isProtectedRoute(req)) {
+//         console.log("Protected route detected:", req.url); // Add logging
+//         auth().protect();
+//         return;
+//     }
+// });
 
-export const config = {
-    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+// export const config = {
+//     matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
